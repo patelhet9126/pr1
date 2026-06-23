32f7cf40d67ae420a4e7151b836b102e48cce388
patelhet9126 <patelhet9126@gmail.com>
2026-06-23 14:48:17 +0530

    Initial commit

---
+++ src/pr11.kt
@@ -0,0 +1,37 @@
+fun main(){
+    var i = 10
+    var j : Int = 20
+    var f : Float = 1.5f
+    var c : Char = 'h'
+    var s : String = "het"
+    var b : Boolean = true
+    var d : Double = 45.0
+    var l : Long = 2345
+    var sh : Short = -98
+    var by : Byte = 123
+
+    var x = 100
+    j = 200
+    x = 300
+
+    println("x = $x")
+
+    println("i = "+i)
+    println("j = "+j)
+    println("f = "+f)
+    println("c = "+c)
+    println("s = "+s)
+    println("b = "+b)
+    println("d = "+d)
+    println("l = "+l)
+    println("sh = "+sh)
+    println("by = "+by)
+
+
+    val name = "Het"
+    val age = 20
+
+    print(age)
+    System.out.printf("Name: %s, Age: %d%n", name, age)
+
+}
---
+++ src/pr12.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val i : Int = 10
+    val j : Double = i.toDouble()
+    val s : String = "1000"
+    val a : Int = s.toInt()
+
+    println(i)
+    println(j)
+    println("string : $s")
+    println("int: $a")
+
+
+}
